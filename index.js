ab812be824ee0aa3d8b3f84eb7723c79172b14f9
dastardlyjockey <fortuneonukansi2015@gmail.com>
2023-12-09 16:30:47 +0100

    initializing the project

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+
+/* CONFIGURATION */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+/* AUTHENTICATION AND ROUTES */
+app.post("/auth/register", register);
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 6001;
+
+mongoose
+  .connect(process.env.MONGO_DB)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server listening on Port: ${PORT}`));
+  })
+  .catch((err) => console.log(`Error connecting to the database: ${err} `));
