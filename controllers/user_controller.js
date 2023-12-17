1e3193c07f7121ac8ebc62c7c0b1d7166699065b
dastardlyjockey <fortuneonukansi2015@gmail.com>
2023-12-17 21:46:42 +0100

    edited the getUser controller to remove the password

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -7,9 +7,17 @@ export const getUsers = async (req, res) => {
 
     const users = await User.find().skip(skip).limit(limit);
 
+    let usersWithoutPassword = [];
+
+    for (let user of users) {
+      const userWithoutPassword = { ...user._doc };
+      delete userWithoutPassword.password;
+      usersWithoutPassword.push(userWithoutPassword);
+    }
+
     const totalCount = await User.countDocuments();
 
-    res.status(200).json({ totalCount, users });
+    res.status(200).json({ totalCount, users: usersWithoutPassword });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
